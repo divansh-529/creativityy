d2e22acf9b53b5c2c5e860f8e50c7a103470e945
divansh-529 <divanshsachdev@gmail.com>
2024-10-03 12:53:58 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+document.getElementById("changeTheme").addEventListener("click", function() {
+    // Select the body element
+    const body = document.body;
+  
+    // Toggle between light and dark themes
+    if (body.style.backgroundColor === "black") {
+      body.style.backgroundColor = "#f0f0f0";
+      body.style.color = "black";
+    } else {
+      body.style.backgroundColor = "black";
+      body.style.color = "black";
+    }
+  });
+  
+
+  document.getElementById("changeTheme-2").addEventListener("click", function() {
+    // Select the body element
+    const body = document.body;
+  
+    // Toggle between light and dark themes
+    if (body.style.backgroundColor === "blue") {
+      body.style.backgroundColor = "#f0f0f0";
+      body.style.color = "blue";
+    } else {
+      body.style.backgroundColor = "blue";
+      body.style.color = "blue";
+    }
+  });
+
+  document.getElementById("changeTheme-3").addEventListener("click", function() {
+    // Select the body element
+    const body = document.body;
+  
+    // Toggle between light and dark themes
+    if (body.style.backgroundColor === "brown") {
+      body.style.backgroundColor = "#f0f0f0";
+      body.style.color = "brown";
+    } else {
+      body.style.backgroundColor = "brown";
+      body.style.color = "brown";
+    }
+  });
+
+  document.getElementById("changeTheme-4").addEventListener("click", function() {
+    // Select the body element
+    const body = document.body;
+  
+    // Toggle between light and dark themes
+    if (body.style.backgroundColor === "#f0f0f0") {
+      body.style.backgroundColor = "#f0f0f0";
+      body.style.color = "black";
+    } else {
+      body.style.backgroundColor = "#f0f0f0";
+      body.style.color = "black";
+    }
+  });
+  
